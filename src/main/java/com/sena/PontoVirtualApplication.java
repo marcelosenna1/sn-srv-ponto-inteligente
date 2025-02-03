f04cf4cd7e032f0209e998bdd9e67625d91d1a1a
Marcelo Sena <marcelo.sena@capgemini.com>
2025-02-02 23:56:10 -0300

    Commit incial

---
+++ src/main/java/com/sena/PontoVirtualApplication.java
@@ -0,0 +1,13 @@
+package com.sena;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PontoVirtualApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PontoVirtualApplication.class, args);
+	}
+
+}
